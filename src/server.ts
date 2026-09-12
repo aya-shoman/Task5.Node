05635519649898e6a90c713239dea5efaf5306aa
Aya Shoman <ayaabdelhamid567@gmail.com>
2026-09-12 21:03:58 +0300

    Prepare Task 5 for Vercel deployment

--- src/server.ts
+++ src/server.ts
@@ -4,22 +4,18 @@ import app from "./app.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 if (!MONGO_URI) {
   throw new Error("MONGO_URI is not defined");
 }
 
-mongoose
-  .connect(MONGO_URI)
+mongoose.connect(MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
-
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
   })
   .catch((error) => {
     console.error("MongoDB connection error:", error);
   });
+
+export default app;
